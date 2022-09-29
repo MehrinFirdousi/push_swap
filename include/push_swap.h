2419c0ddf2046d5816d514a77cb3fde551ede951
Mehrin Firdousi <mfirdous@lab3r3s10.42abudhabi.ae>
2022-09-29 12:12:00 +0400

    atoi modified with overflow detection

--- include/push_swap.h
+++ include/push_swap.h
@@ -21,6 +21,8 @@
 # endif
 
 # include <stdio.h>
+# include <stdlib.h>
+# include <limits.h>
 # include "libft.h"
 
 #endif
--- srcs/push_swap.c
+++ srcs/push_swap.c
@@ -15,9 +15,9 @@
 // atoi with overflow detection
 int	ft_atoi_ofd(const char *nptr)
 {
-	int	i;
-	int	sum;
-	int	sign;
+	long long int	sum;
+	int				i;
+	int				sign;
 
 	i = 0;
 	sum = 0;
@@ -29,66 +29,66 @@ int	ft_atoi_ofd(const char *nptr)
 		if (nptr[i] == '-')
 			sign = -1;
 		i++;
-	}	
+	}
 	while (nptr[i] >= 48 && nptr[i] <= 57)
 	{
 		sum = (sum * 10) + (nptr[i++] - 48);
-		printf("sum = %d\n", sum);
-		if (sum <= 0) // overflow has occurred except for when the number is INT_MIN
-		 	return (0);
+		if ((sign == 1 && sum > INT32_MAX) || (sign == -1 && sum > 2147483648))
+			return (0);
 	}
+	if (nptr[i] && !ft_isdigit(nptr[i]))
+		return (0);
 	return (sign * sum);
 }
 
-// int *create_stack_a(int len, char **argv)
-// {
-// 	int	*a;
-// 	int	i;
-// 	int	j;
-
-// 	a = (int *)malloc(sizeof(int) * len);
-// 	if (!a)
-// 		return (NULL);
-// 	i = -1;
-// 	while (argv[++i])
-// 	{
-// 		a[i] = ft_atoi(argv[i]);
-// 		if (a[i] == 0 && argv[i][0] != '0') // not an integer
-// 		{
-// 			free(a);
-// 			ft_printf("Error\n");
-// 			return (NULL);
-// 		}
-// 		j = -1;
-// 		while (++j < i) // duplicate exists
-// 			if (a[i] == a[j])
-// 			{
-// 				free(a);
-// 				ft_printf("Error\n");
-// 				return (NULL);
-// 			}
-
-
-// 	}
-// 	return (a);
-// }
-
-int main(int argc, char **argv)
+int	*create_stack_a(int len, char **argv)
 {
-	// int *a;
-	// int *b;
+	int	*a;
+	int	i;
+	int	j;
+
+	a = (int *)malloc(sizeof(int) * len);
+	if (!a)
+		return (NULL);
+	i = -1;
+	while (argv[++i])
+	{
+		a[i] = ft_atoi_ofd(argv[i]);
+		if (a[i] == 0 && argv[i][0] != '0')
+		{
+			free(a);
+			return (NULL);
+		}
+		j = -1;
+		while (++j < i)
+			if (a[i] == a[j])
+			{
+				free(a);
+				return (NULL);
+			}
+	}
+	return (a);
+}
+
+int	main(int argc, char **argv)
+{
+	int	*a;
+	int	*b;
+	int	i;
 
 	if (argc == 1)
 		return (0);
-	//a = create_stack_a(argc - 1, argv + 1);
-	printf("%d\n", ft_atoi_ofd(argv[1]));
+	a = create_stack_a(argc - 1, argv + 1);
+	if (!a)
+	{
+		ft_printf("Error\n");
+		return (EXIT_FAILURE);
+	}
+	i = -1;
+	while (++i < argc -1)
+		printf("%d\n", a[i]);
+	b = (int *)malloc(sizeof(int) * argc - 1);
+	free(a);
+	free(b);
 	return (0);
 }
-
-/*
-	errors:
-	1. no args - just return 
-	2. args aren't integers
-	3. > INT_MAX or < INT_MIN
-	4. there are duplicates
-*/
